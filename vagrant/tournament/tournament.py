7a8f91e931f4d9158462502ef11398f355033d6e
tboquet <thomas.boquet@hec.ca>
2015-11-04 21:41:59 -0500

    + space

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -7,6 +7,8 @@ import psycopg2
 import bleach
 
 
+
+
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=tournament")
