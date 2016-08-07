e2e2b10b9bd4f6433431d6843dfbd5a7b1a15721
tboquet <thomas.boquet@hec.ca>
2016-08-07 16:13:37 -0400

    + docstring

--- vagrant/catalog/catalogitem.py
+++ vagrant/catalog/catalogitem.py
@@ -41,6 +41,7 @@ session = DBSession()
 # Create anti-forgery state token
 @app.route('/login')
 def showLogin():
+    """Render the login page"""
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in range(32))
     login_session['state'] = state
@@ -50,7 +51,8 @@ def showLogin():
 
 @app.route('/fbconnect', methods=['POST'])
 def fbconnect():
-    """Function retrieved from
+    """Connection via facebook
+    Function retrieved from
     https://github.com/udacity/ud330/blob/master/Lesson4/step2/project.py"""
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
@@ -125,6 +127,7 @@ def fbconnect():
 
 @app.route('/fbdisconnect')
 def fbdisconnect():
+    """Disconnect a user connected via facebook"""
     facebook_id = login_session['facebook_id']
     # The access token must me included to successfully logout
     access_token = login_session['access_token']
@@ -137,6 +140,7 @@ def fbdisconnect():
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    """Connect a user via facebook"""
     # Validate state token
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
@@ -235,6 +239,7 @@ def gconnect():
 # User Helper Functions
 
 def createUser(login_session):
+    """Create a user in the db based on the info stored in the session"""
     newUser = User(name=login_session['username'], email=login_session[
                    'email'], picture=login_session['picture'])
     session.add(newUser)
@@ -244,22 +249,25 @@ def createUser(login_session):
 
 
 def getUserInfo(user_id):
+    """Get the user infos from the db given a user id"""
     user = session.query(User).filter_by(id=user_id).one()
     return user
 
 
 def getUserID(email):
+    """Get the user id from the db given an email adress"""
     try:
         user = session.query(User).filter_by(email=email).one()
         return user.id
     except:
         return None
 
-# DISCONNECT - Revoke a current user's token and reset their login_session
 
+# DISCONNECT - Revoke a current user's token and reset their login_session
 
 @app.route('/gdisconnect')
 def gdisconnect():
+    """Disconnect a user connected using google"""
     # Only disconnect a connected user.
     credentials = login_session.get('credentials')
     if credentials is None:
@@ -282,6 +290,7 @@ def gdisconnect():
 # Disconnect based on provider
 @app.route('/disconnect')
 def disconnect():
+    """Disconnect the user"""
     if 'provider' in login_session:
         if login_session['provider'] == 'google':
             gdisconnect()
@@ -304,6 +313,10 @@ def disconnect():
 
 @app.route('/category/<string:category_name>/JSON')
 def categoryCatalogJSON(category_name):
+    """Return a json with all categories and their attributes
+
+    Returns:
+        A json of all categories"""
     items = session.query(
         CatalogItem, Category).filter(and_(
             Category.name == category_name,
@@ -314,12 +327,21 @@ def categoryCatalogJSON(category_name):
 
 @app.route('/category/all/JSON')
 def categoryCatalogJSONall():
+    """Return a json with all categories and their attributes
+
+    Returns:
+        A json of all categories"""
     items = session.query(CatalogItem).all()
     return jsonify(CatalogItems=[i.serialize for i in items])
 
 
 @app.route('/category/<string:category_name>/<string:item_name>/JSON')
 def catalogItemJSON(category_name, item_name):
+    """Return a json with all categories and their attributes
+
+    Returns:
+        A json of all categories"""
+    # we filter by item name and sort by date added in the db
     item = session.query(
         CatalogItem, Category).filter(and_(
             Category.name == category_name,
@@ -331,6 +353,10 @@ def catalogItemJSON(category_name, item_name):
 
 @app.route('/category/JSON')
 def categoriesJSON():
+    """Return a json with all categories and their attributes
+
+    Returns:
+        A json of all categories"""
     categories = session.query(Category).all()
     return jsonify(categories=[r.serialize for r in categories])
 
@@ -339,6 +365,12 @@ def categoriesJSON():
 @app.route('/')
 @app.route('/category/')
 def showCategories():
+    """Show categories and items
+
+    Render the homepage.
+
+    Returns:
+        render the categories template if a GET request is sent"""
     categories = session.query(Category).all()
     items = session.query(
         CatalogItem, Category).filter(
@@ -351,6 +383,16 @@ def showCategories():
 # Create a new category
 @app.route('/category/new/', methods=['GET', 'POST'])
 def newCategory():
+    """Create a category
+
+    Need to be logged in to be able to see the edit page.
+
+    Args:
+        category_name(str): the name of the category the object belongs to
+
+    Returns:
+        render the newCategory template if a GET request is sent
+        redirect to the home page if the POST request succeeds"""
     if 'provider' in login_session:
         form = FormCategory()
         if form.validate_on_submit():
@@ -367,6 +409,16 @@ def newCategory():
 
 @app.route('/category/<string:category_name>/edit/', methods=['GET', 'POST'])
 def editCategory(category_name):
+    """Edit a category
+
+    Need to be logged in to be able to see the edit page.
+
+    Args:
+        category_name(str): the name of the category the object belongs to
+
+    Returns:
+        render the editCategory template if a GET request is sent
+        redirect to the home page if the POST request succeeds"""
     if 'provider' in login_session:
         editedCategory = session.query(
             Category).filter_by(name=category_name).one()
@@ -387,6 +439,16 @@ def editCategory(category_name):
 
 @app.route('/category/<string:category_name>/delete/', methods=['GET', 'POST'])
 def deleteCategory(category_name):
+    """Delete a category
+
+    Need to be logged in to be able to see the delete page.
+
+    Args:
+        category_name(str): the name of the category the object belongs to
+
+    Returns:
+        render the deleteCategory template if a GET request is sent
+        redirect to the home page if the POST request succeeds"""
     if 'provider' in login_session:
         categoryToDelete = session.query(
             Category).filter_by(name=category_name).one()
@@ -405,6 +467,13 @@ def deleteCategory(category_name):
 
 @app.route('/category/<string:category_name>/')
 def showCategory(category_name):
+    """Show a category and all its items
+
+    Args:
+        category_name(str): the name of the category the item belongs to
+
+    Returns:
+        render the catalog template"""
     category = session.query(Category).filter_by(name=category_name).one()
     categories = session.query(Category).all()
     items = session.query(CatalogItem).filter_by(
@@ -420,6 +489,14 @@ def showCategory(category_name):
 
 @app.route('/category/<string:category_name>/<string:item_name>')
 def showCatalogItem(category_name, item_name):
+    """Show an item and all its attributes
+
+    Args:
+        category_name(str): the name of the category the item belongs to
+        item_name(str): the name of the item
+
+    Returns:
+        render the catalogitem template"""
     item = session.query(CatalogItem).filter_by(name=item_name).one()
     category = session.query(Category).filter_by(name=category_name).one()
     return render_template('catalogitem.html', item=item,
@@ -429,6 +506,14 @@ def showCatalogItem(category_name, item_name):
 @app.route('/category/<string:category_name>/new/',
            methods=['GET', 'POST'])
 def newCatalogItem(category_name):
+    """Create a new item in the database
+
+    Args:
+        category_name(str): the name of the category the item belongs to
+
+    Returns:
+        render the newcatalogitem template if a GET request is sent
+        redirect to the category page if the POST request succeeds"""
     if 'provider' in login_session:
         form = FormItem()
         if form.validate_on_submit():
@@ -452,6 +537,15 @@ def newCatalogItem(category_name):
 @app.route('/category/<string:category_name>/<string:item_name>/edit',
            methods=['GET', 'POST'])
 def editCatalogItem(category_name, item_name):
+    """Edit the attributes of an item in the database
+
+    Args:
+        category_name(str): the name of the category the item belongs to
+        item_name(str): the name of the item
+
+    Returns:
+        render the editcatalogitem template if a GET request is sent
+        redirect to the category page if the POST request succeeds"""
     if 'provider' in login_session:
         editedItem = session.query(CatalogItem).filter_by(name=item_name).one()
         form = FormItem()
@@ -474,14 +568,21 @@ def editCatalogItem(category_name, item_name):
     else:
         flash("Please login to be able to edit an item")
         return redirect(url_for('showCategory', category_name=category_name))
-    # return 'This page is for editing catalog item %s' % item_id
-
-    # Delete a catalog item
 
 
 @app.route('/category/<string:category_name>/<string:item_name>/delete',
            methods=['GET', 'POST'])
 def deleteCatalogItem(category_name, item_name):
+    """Deletes an item given a category and an item name
+    Need to be logged in to be able to see the delete page.
+
+    Args:
+        category_name(str): the name of the category the object belongs to
+        item_name(str): the name of the item
+
+    Returns:
+        render the deletecatalogitem template if a GET request is sent
+        redirect to the category page if the POST request succeeds"""
     if 'provider' in login_session:
         itemToDelete = session.query(CatalogItem).filter_by(
             name=item_name).one()
@@ -496,7 +597,6 @@ def deleteCatalogItem(category_name, item_name):
     else:
         flash("Please login to be able to delete an item")
         return redirect(url_for('showCategory', category_name=category_name))
-        # return "This page is for deleting catalog item %s" % item_id
 
 
 if __name__ == '__main__':
